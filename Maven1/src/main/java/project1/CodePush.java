9569c5cde533e0ba5c09db19b4507e55a2b26304
3DiQNaveen <80444474+3DiQNaveen@users.noreply.github.com>
2021-08-19 12:52:41 +0530

    first commit

---
+++ Maven1/src/main/java/project1/CodePush.java
@@ -0,0 +1,13 @@
+package project1;
+
+import org.testng.annotations.Test;
+
+public class CodePush {
+	
+	@Test
+	public void push()
+	{
+		System.out.println("Push eclipse code to  github");
+	}
+
+}
